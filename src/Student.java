85f058b098cd56f2cbce847e3ec2f112712bfb33
Arfi <arma0001@stud.kea.dk>
2023-11-15 11:03:57 +0100

    Hello

---
+++ src/Student.java
@@ -0,0 +1,29 @@
+public class Student implements Comparable<Student>{
+    private String name;
+    private int zipCode;
+    private boolean livingWithParents;
+    private  char gender;
+
+
+    public Student(String name, int zipCode, boolean livingWithParents, char gender) {
+        this.name = name;
+        this.zipCode = zipCode;
+        this.livingWithParents = livingWithParents;
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", zipCode=" + zipCode +
+                ", livingWithParents=" + livingWithParents +
+                ", gender=" + gender +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return this.name.compareTo(o.name);
+    }
+}
---
+++ src/StudentSorter.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class StudentSorter {
+    ArrayList <Student> students;
+
+    public static void main(String[] args) {
+        new StudentSorter().run();
+    }
+
+    private void run() {
+        students = new ArrayList<>();
+        students.add(new Student("Anders", 2200, true, 'M'));
+        students.add(new Student("Patrik", 2400, false, 'M'));
+        students.add(new Student("Lili", 2000, true, 'F'));
+        students.add(new Student("Kat", 3400, false, 'F'));
+        students.add(new Student("Hat", 2300, false, 'M'));
+        students.add(new Student("Arfi", 5600, true, 'F'));
+        students.add(new Student("Ifra", 6700, false, 'F'));
+        students.add(new Student("Asha", 7820, true, 'F'));
+        students.add(new Student("Hana", 3455, true, 'F'));
+        students.add(new Student("Hanadi", 8899, false, 'F'));
+        students.add(new Student("Andy", 3200, true, 'M'));
+
+        for (int i= 0; i < students.size(); i ++){
+            System.out.println(students.get(i));
+        }
+        Collections.sort(students);
+        System.out.println("After sorting");
+        for (int i= 0; i < students.size(); i ++){
+            System.out.println(students.get(i));
+        }
+    }
+
+}
